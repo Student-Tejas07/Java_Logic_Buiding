410f3083c90cdc85a1dcaecd71316f3113bc8b97
Student-Tejas07 <tejas.ingale2005@gmail.com>
2026-01-22 22:44:32 +0530

    Basic

---
+++ src/main/java/org/example/day4/print1_n.java
@@ -0,0 +1,24 @@
+package org.example.day4;
+import java.util.Scanner;
+public class print1_n {
+
+    static void printnums(int n){
+
+        if (n==0){
+            return;
+        }
+        printnums(n-1); //this is recursion
+
+        System.out.println(n + " ");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter a Number: ");
+        int n=sc.nextInt();
+
+        printnums(n);
+
+    }
+}
